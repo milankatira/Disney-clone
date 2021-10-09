6fbca30087531773e31734b7759283e9a74cda59
milankatira <milankatira26@gmail.com>
2021-10-09 13:30:32 +0530

    login page

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Header from './components/Header';
 import Home from './components/Home';
 import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
 import Detail from './components/Detail';
+import Login from './components/Login';
 function App() {
   return (
     <div className="App">
@@ -16,6 +17,9 @@ function App() {
           </Route>
           <Route path="/home" >
             <Home />
+          </Route>
+          <Route path="/login" >
+            <Login/>
           </Route>
         </Switch>
       </Router>
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,11 +1,88 @@
 import React from 'react'
-
+import styled from 'styled-components';
 const Login = () => {
     return (
-        <div>
-            
-        </div>
+        <Container>
+            <CTA>
+                <CTALogoOne src="/images/cta-logo-one.svg" />
+                <SignUp>
+                    GET ALL THERE
+                </SignUp>
+                <Description>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. A facilis beatae iste fugiat sunminima,accusantium
+                    ducimus error quidem dicta dolorum asperiores. Eum alias dolores inventore iusto at vero iure? Eveniet vitae rerum architecto eligendi saepe, corporis non voluptates repellat.
+                </Description>
+                <CTALogoTwo src="/images/cta-logo-two.png" />
+            </CTA>
+        </Container>
     )
 }
 
 export default Login
+
+const Container = styled.div`
+display: flex;
+align-items: top;
+justify-content:center;
+position: relative;
+height:calc(100vh - 70px);
+background-image: url("/images/login-background.jpg");
+
+&:before {
+background-position: top;
+background-size: cover;
+background-repeat: no-repeat;
+background-image: url("images/login-background.jpg");
+position: absolute;
+top: 0;
+bottom:0;
+left: 0;
+right: 0;
+z-index: -1;
+}
+`
+
+const CTA = styled.div`
+max-width:650px;
+padding:80px 40px;
+width:90%;
+display: flex;
+flex-direction:column;
+align-items: center;
+`
+const CTALogoOne = styled.img`
+
+`
+
+const CTALogoTwo = styled.img`
+width:90%;
+
+`
+
+
+const SignUp = styled.a`
+width:100%;
+background-color:#0063e5;
+font-weight:bold;
+padding:17px 0;
+color:#f9f9f9;
+border-radius:4px solid;
+text-align:center;
+font-size:18px;
+cursor: pointer;
+transition: all 250ms cubic-bezier(0.25, 0.45,0.45, 0.94)0s;
+letter-spacing:0.5px;
+margin-top: 8px;
+margin-bottom:12px;
+
+&:hover{
+background-color:#0483ee;
+}
+`
+
+const Description = styled.p`
+font-size:11px;
+letter-spacing:0.5px;
+text-align:center;
+line-height:1.5;
+`
